b0b8c3363b4cbf36fb001444270ef4377ecc3c0f
mm554 <murawskim994@gmail.com>
2024-09-24 13:06:50 +0200

    Working on login

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './index.css';
 import './Login.css'
 export default function Test() {
+    function logg(formData) {
+        console.log(formData)
+    }
     return (
         <div>
-           <label htmlFor="email">Email: </label>
-           <input type="email" name="email" id="email" />
-           <label htmlFor="password">Password: </label>
-           <input type="password" name="password" id="password" />
+            <form action={logg}>
+                <label htmlFor="email">Email: </label><br />
+                <input type="email" name="email" id="email" /><br />
+                <label htmlFor="password">Password: </label><br />
+                <input type="password" name="password" id="password" /><br /><br />
+                <input type="submit"></input>
+           </form>
         </div>
     );
 }
