020c9ca8c508b7c355767428a6ae52da1d51f53a
Szymon Marcinkiewicz/smarcinkiewicz88 <szymonmarcinkiewicz2008@gmail.com>
2024-10-08 12:18:32 +0200

    modified register

--- src/register.jsx
+++ src/register.jsx
@@ -3,27 +3,28 @@ import './index.css';
 import './Register.css'
 export default function Register() {
     return (
-        
+        <div className='register'>
             <form action="">
-                <label htmlFor="email"></label>
-                <input type="text" name='email' id='email'/> <br />
-                <label htmlFor="password"></label>
-                <input type="text" name='password' id='password'/> <br />
-                <label htmlFor="Confirm password"></label>
-                <input type="text" name='Confirm password' id='confirmPassword'/> <br />
-                <label htmlFor="username"></label>
-                <input type="text" name='username' id='username'/> <br />
-                <label htmlFor="name"></label>
-                <input type="text" name='name' id='name'/> <br />
-                <label htmlFor="surname"></label>
-                <input type="text" name='surname' id='surname'/> <br />
-                <label htmlFor="Home adress"></label>
-                <input type="text" name='Home adress' id='homeAdress'/> <br />
-                <label htmlFor="age"></label> 
-                <input type="text" name='age' id='age'/> <br />
-                <label htmlFor="Phone number"></label>
-                <input type="text" name='Phone number' id='phoneNumber'/> <br />
+                <label htmlFor="email">Email: </label><br/>
+                <input type="text" name='email' id='email'/> <br/>
+                <label htmlFor="password">Password: </label><br/>
+                <input type="text" name='password' id='password'/><br/>
+                <label htmlFor="Confirm password">Confirm password:</label><br/>
+                <input type="text" name='Confirm password' id='confirmPassword'/><br/>
+                <label htmlFor="username"></label><br/>
+                <input type="text" name='username' id='username'/> <br/>
+                <label htmlFor="name"></label><br/>
+                <input type="text" name='name' id='name'/><br/>
+                <label htmlFor="surname"></label><br/>
+                <input type="text" name='surname' id='surname'/> <br/>
+                <label htmlFor="Home adress"></label><br/>
+                <input type="text" name='Home adress' id='homeAdress'/> <br/>
+                <label htmlFor="age"></label> <br/>
+                <input type="text" name='age' id='age'/><br/>
+                <label htmlFor="Phone number"></label><br/>
+                <input type="text" name='Phone number' id='phoneNumber'/> <br/>
             </form>
            
+        </div>
     );
 }
