083d75ab398a9a092f597b51bb0f71ca79ae6a3d
syi0 <syi0.v006@gmail.com>
2024-10-08 12:10:15 +0200

    g

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Login from './Login';
-import Register from './Register';
+import Register from './register';
 import {
   createBrowserRouter,
   RouterProvider,
