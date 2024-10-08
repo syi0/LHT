b5a070cdbb795f298c465373293b470b318ff841
syi0 <syi0.v006@gmail.com>
2024-10-08 12:11:57 +0200

    g

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,11 +3,11 @@ import './index.css';
 import './Login.css'
 export default function Test() {
     return (
-        <div>
+        <form action="">
            <label htmlFor="email">Email: </label>
            <input type="email" name="email" id="email" />
            <label htmlFor="password">Password: </label>
            <input type="password" name="password" id="password" />
-        </div>
+        </form>
     );
 }
--- src/body/body.jsx
+++ src/body/body.jsx
@@ -7,14 +7,14 @@ import './body.css'
                     <img src="bg-photo" alt="" />
                 </section>
                 <section className='Opis'>
-                <h1>Witamy w BigService</h1>
-                <p>To </p>
-                </section>
-                <section className='logowanie'>
-                <h2>Tutaj mozemy dac duzy przycik typu zaloguj lub zajerestruj sie</h2>
-                <button>Zaloguj się</button>
-                <h3>Lub</h3>
-                <button>Zarejestruj się</button>
+                    <h1>Witamy w BigService</h1>
+                    <p>To </p>
+                    </section>
+                    <section className='logowanie'>
+                    <h2>Tutaj mozemy dac duzy przycik typu zaloguj lub zajerestruj sie</h2>
+                    <button>Zaloguj się</button>
+                    <h3>Lub</h3>
+                    <button>Zarejestruj się</button>
                 </section>
                 
             </div>
