6e371ed9f845cd1798baa509f5958980513ec0ec
spiekarski18 <dragonoffire323@gmail.com>
2024-10-08 12:33:37 +0200

    register

--- src/register.jsx
+++ src/register.jsx
@@ -11,17 +11,17 @@ export default function Register() {
                 <input type="text" name='password' id='password'/><br/>
                 <label htmlFor="Confirm password">Confirm password:</label><br/>
                 <input type="text" name='Confirm password' id='confirmPassword'/><br/>
-                <label htmlFor="username"></label><br/>
+                <label htmlFor="username">Username</label><br/>
                 <input type="text" name='username' id='username'/> <br/>
-                <label htmlFor="name"></label><br/>
+                <label htmlFor="name">Name</label><br/>
                 <input type="text" name='name' id='name'/><br/>
-                <label htmlFor="surname"></label><br/>
+                <label htmlFor="surname">Surname</label><br/>
                 <input type="text" name='surname' id='surname'/> <br/>
-                <label htmlFor="Home adress"></label><br/>
+                <label htmlFor="Home adress">Home adress</label><br/>
                 <input type="text" name='Home adress' id='homeAdress'/> <br/>
-                <label htmlFor="age"></label> <br/>
+                <label htmlFor="age">Age</label> <br/>
                 <input type="text" name='age' id='age'/><br/>
-                <label htmlFor="Phone number"></label><br/>
+                <label htmlFor="Phone number">Phone number</label><br/>
                 <input type="text" name='Phone number' id='phoneNumber'/> <br/>
             </form>
            
