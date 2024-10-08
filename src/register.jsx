052a000d4678d315807ae0f2e3ad4d74cdb82240
spiekarski18 <dragonoffire323@gmail.com>
2024-10-08 13:07:58 +0200

    ssds

--- src/register.jsx
+++ src/register.jsx
@@ -26,23 +26,23 @@ export default function Register() {
     return (
         <div className='register' onSubmit={handleSubmit}>
             <form action="">
-                <label htmlFor="email">Email: </label><br/>
+                <label htmlFor="email">Email: </label>
                 <input type="text" name='email' id='email'/> <br/>
-                <label htmlFor="password">Password: </label><br/>
+                <label htmlFor="password">Password: </label>
                 <input type="text" name='password' id='password'/><br/>
-                <label htmlFor="Confirm password">Confirm password:</label><br/>
+                <label htmlFor="Confirm password">Confirm password:</label>
                 <input type="text" name='Confirm password' id='confirmPassword'/><br/>
-                <label htmlFor="username">Username</label><br/>
+                <label htmlFor="username">Username</label>
                 <input type="text" name='username' id='username'/> <br/>
-                <label htmlFor="name">Name</label><br/>
+                <label htmlFor="name">Name</label>
                 <input type="text" name='name' id='name'/><br/>
-                <label htmlFor="surname">Surname</label><br/>
+                <label htmlFor="surname">Surname</label>
                 <input type="text" name='surname' id='surname'/> <br/>
                 <label htmlFor="Home adress">Home adress</label><br/>
                 <input type="text" name='Home adress' id='homeAdress'/> <br/>
-                <label htmlFor="age">Age</label> <br/>
+                <label htmlFor="age">Age</label>
                 <input type="text" name='age' id='age'/><br/>
-                <label htmlFor="Phone number">Phone number</label><br/>
+                <label htmlFor="Phone number">Phone number</label>
                 <input type="text" name='Phone number' id='phoneNumber'/> <br/>
                 <input type="submit" />
             </form>
