61cb7705c56562d8e52fb3c1511f76ede48af101
spiekarski18 <dragonoffire323@gmail.com>
2024-10-08 12:19:29 +0200

    footer

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -4,11 +4,11 @@ import './footer.css'
         return(
             <footer className = 'linkList'>
                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Shop</a></li>
-                    <li><a href="https://github.com/syi0/LHT/">GitHub</a></li>
-                    <li><a href="#">Login</a></li>
-                    <li><a href="#">Register</a></li>
+                    <li><a href="#"><i class="fa-solid fa-house"></i>Home</a></li>
+                    <li><a href="#"><i class="fa-solid fa-shop"></i>Shop</a></li>
+                    <li><a href="https://github.com/syi0/LHT/"><i class="fa-brands fa-github"></i>GitHub</a></li>
+                    <li><a href="#"><i class="fa-solid fa-user"></i>Login</a></li>
+                    <li><a href="#"><i class="fa-regular fa-user"></i>Register</a></li>
                </ul>
             </footer>
         );
