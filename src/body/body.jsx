e562e45664810b24dfdde8b04377dd20c2128f34
spiekarski18 <dragonoffire323@gmail.com>
2024-10-08 11:59:45 +0200

    Update body.jsx

--- src/body/body.jsx
+++ src/body/body.jsx
@@ -3,9 +3,22 @@ import './body.css'
     function Body(){
         return(
             <div className = 'body'>
-                <h1>Tu powinna byc strona</h1>
+                <section className='photot'>
+                    <img src="bg-photo" alt="" />
+                </section>
+                <section className='Opis'>
+                <h1>Coś w stylu witamy na naszej strone</h1>
+                <p>Tutaj jakiś opis co robimy</p>
+                </section>
+                <section className='logowanie'>
+                <h2>Tutaj mozemy dac duzy przycik typu zaloguj lub zajerestruj sie</h2>
+                <button>Zaloguj się</button>
+                <p>Lub</p>
+                <button>Zarejestruj się</button>
+                </section>
+                
             </div>
         );
-    }
+    }S
 
 export default Body;
