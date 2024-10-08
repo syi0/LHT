18e303bbcbce02dd7f327f525fdfa66b38ded2b5
syi0 <syi0.v006@gmail.com>
2024-10-08 12:00:36 +0200

    G

--- src/Nav/NavSmall.jsx
+++ src/Nav/NavSmall.jsx
@@ -2,6 +2,16 @@ import './Nav.css';
 
 function NavSmall() {
     return(
-        <div></div>
+        <nav className='navListSmall'>
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-house"></i></a></li>
+                <li><a href="#"><i class="fa-solid fa-shop"></i></a></li>
+                <li><a href="https://github.com/syi0/LHT/"><i class="fa-brands fa-github"></i></a></li>
+                <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
+                <li><a href="#"><i class="fa-regular fa-user"></i></a></li>
+            </ul>
+        </nav>
     );
 }
+
+export default NavSmall
