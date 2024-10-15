72c3e6acd663dbc93b5b2225250da3ffc2147258
syi0 <syi0.v006@gmail.com>
2024-10-15 13:06:25 +0200

    GGGG

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -4,11 +4,11 @@ function Nav() {
     return(
         <nav>
             <ul className='navList'>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Shop</a></li>
+                <li><a href="#">Home <i class="fa-solid fa-house"></i></a></li>
+                <li><a href="#">Shop <i class="fa-solid fa-shop"></i></a></li>
                 <li><a href="https://github.com/syi0/LHT/">GitHub <i class="fa-brands fa-github"></i></a></li>
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Register</a></li>
+                <li><a href="#">Login <i class="fa-solid fa-user"></i></a></li>
+                <li><a href="#">Register <i class="fa-regular fa-user"></i></a></li>
             </ul>
         </nav>
     );
--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -8,7 +8,7 @@ import './footer.css'
                     <li><a href="#"><i class="fa-solid fa-shop"></i>Shop</a></li>
                     <li><a href="https://github.com/syi0/LHT/"><i class="fa-brands fa-github"></i>GitHub</a></li>
                     <li><a href="#"><i class="fa-solid fa-user"></i>Login</a></li>
-                    <li><a href="#"><i class="fa-regular fa-user"></i>Register</a></li>
+                    <li><a href="#"><i class="fa-regular fa-user"></i>Register </a></li>
                </ul>
             </footer>
         );
