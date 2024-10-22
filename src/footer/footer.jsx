6a17f01294b6aaf0716366ab97caa149fae80f6d
spiekarski18 <dragonoffire323@gmail.com>
2024-10-22 12:39:50 +0200

    g

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -4,9 +4,9 @@ import './footer.css'
         return(
             <footer className = 'linkList'>
                <ul>
-                <li><i class="fa-solid fa-envelope"></i>BigService@adres.com</li>
-                <li><i class="fa-solid fa-phone"></i>+48 xxxyyyzzz</li>
-                <li><i class="fa-regular fa-copyright"></i>BigService</li>
+                <li><i class="fa-solid fa-envelope"></i>BigService@bigservice.com</li>
+                <li><i class="fa-solid fa-phone"></i>+48 123456789</li>
+                <li><i class="fa-regular fa-copyright"></i>BigService&trade; {(new Date().getFullYear())}</li>
                </ul>
             </footer>
         );
