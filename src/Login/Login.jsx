120264add07116c140e6fdfe598d80c205549be1
mm554 <murawskim994@gmail.com>
2024-10-22 12:20:26 +0200

    login backend

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 import '../index.css';
 import './Login.css'
 export default function Test() {
+    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson.email);
+    let qr=`SELECT Id FROM Login WHERE email='${formJson.email}' AND password='${formJson.password}'`;
+    axios.post('/api/db', {
+      query: qr
+     
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+
+}
     return (
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
            <label htmlFor="email">Email: </label>
            <input type="email" name="email" id="email" />
            <label htmlFor="password">Password: </label>
            <input type="password" name="password" id="password" />
+           <button type="submit"></button>
         </form>
     );
 }
