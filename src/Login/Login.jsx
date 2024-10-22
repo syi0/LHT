3750d2642b34869142c4153bd409734029038889
syi0 <syi0.v006@gmail.com>
2024-10-22 11:50:00 +0200

    g

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';
+import '../index.css';
 import './Login.css'
 export default function Test() {
     return (
--- src/Register/register.jsx
+++ src/Register/register.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';
+import '../index.css';
 import axios from 'axios';
 import './Register.css'
 export default function Register() {
@@ -32,23 +32,23 @@ export default function Register() {
     return (
         <div className='register' onSubmit={handleSubmit}>
             <form action="">
-                <label htmlFor="email">Email: </label>
+                <label htmlFor="email">Email: </label> <br/>
                 <input type="text" name='email' id='email'/> <br/>
-                <label htmlFor="password">Password: </label>
+                <label htmlFor="password">Password: </label><br/>
                 <input type="text" name='password' id='password'/><br/>
-                <label htmlFor="Confirm password">Confirm password:</label>
+                <label htmlFor="Confirm password">Confirm password:</label><br/>
                 <input type="text" name='Confirm password' id='confirmPassword'/><br/>
-                <label htmlFor="username">Username</label>
+                <label htmlFor="username">Username</label><br/>
                 <input type="text" name='username' id='username'/> <br/>
-                <label htmlFor="name">Name</label>
+                <label htmlFor="name">Name</label><br/>
                 <input type="text" name='name' id='name'/><br/>
-                <label htmlFor="surname">Surname</label>
+                <label htmlFor="surname">Surname</label><br/>
                 <input type="text" name='surname' id='surname'/> <br/>
                 <label htmlFor="Home adress">Home adress</label><br/>
                 <input type="text" name='Home adress' id='homeAdress'/> <br/>
-                <label htmlFor="age">Age</label>
+                <label htmlFor="age">Age</label><br/>
                 <input type="text" name='age' id='age'/><br/>
-                <label htmlFor="Phone number">Phone number</label>
+                <label htmlFor="Phone number">Phone number</label><br/>
                 <input type="text" name='Phone number' id='phoneNumber'/> <br/>
                 <input type="submit" />
             </form>
