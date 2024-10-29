7e799b1700258348ff0b2269aef9e1efd9e97626
syi0 <syi0.v006@gmail.com>
2024-10-29 12:37:14 +0100

    g

--- src/Shop/Shop.jsx
+++ src/Shop/Shop.jsx
@@ -11,11 +11,14 @@ export default function Shop() {
     return(
         <div className='main'>
             <nav>
-                <li><a href="">x</a></li>
-                <li><a href="">x</a></li>
-                <li><a href="">x</a></li>
-                <li><a href="">x</a></li>
-            </nav>
+            <ul className='navList'>
+                <li><a href="/">Home <i class="fa-solid fa-house"></i></a></li>
+                <li><a href="/shop">Shop <i class="fa-solid fa-shop"></i></a></li>
+                <li><a href="https://github.com/syi0/LHT/">GitHub <i class="fa-brands fa-github"></i></a></li>
+                <li><a href="/login">Login <i class="fa-solid fa-user"></i></a></li>
+                <li><a href="/register">Register <i class="fa-regular fa-user"></i></a></li>
+            </ul>
+        </nav>
             <main>
                 <button>Create your offer!</button>
                 
