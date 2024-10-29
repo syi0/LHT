a8b6a447aa23f45195791ac693cc50b3554eedb1
syi0 <syi0.v006@gmail.com>
2024-10-29 12:14:46 +0100

    G

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -28,7 +28,7 @@ export default function Test() {
             <input type="email" name="email" id="email" /> <br /><br />
             <label htmlFor="password">Password: </label> <br />
             <input type="password" name="password" id="password" /> <br /> <br />
-            <button type="submit">Login</button>
+            <button type="submit">Login!</button>
           </form>
         </div>
     );
